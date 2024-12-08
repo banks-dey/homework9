0f4cf26dcb417f67c576e2c01aeede8f84962577
banks-dey <espatb@gmail.com>
2024-12-08 20:49:17 +0000

    added src

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+const header = document.getElementById('header');
+header.style.backgroundColor = 'blue';
+header.style.color = 'white';
+header.innerHTML = 'Learning How to Manipulate the DOM using JS 💪🏼 ';
+
+const para1 = document.getElementById('para1');
+para1.style.backgroundColor = 'gray';
+para1.style.color = 'black';
+para1.innerHTML = 'I now know how DOM is 🤩 ';
+
+const para2 = document.getElementById('para2');
+para2.style.backgroundColor = 'yellow';
+para2.style.color = 'black';
+para2.innerHTML = 'I know how to select / access elements in the DOM 📚 ';
+
+const para3 = document.getElementById('para3');
+para3.style.backgroundColor = 'green';
+para3.style.color = 'white';
+para3.innerHTML = 'I know to use the style methods to change the style stle of the element in the DOM ✔️';
+
+const para4 = document.getElementById('para4');
+para4.style.backgroundColor = 'purple';
+para4.style.color = 'black';
+para4.innerHTML = 'I know how the styling properties differ in JS from CSS  ';
+
+const para5 = document.getElementById('para5');
+para5.style.backgroundColor = 'black';
+para5.style.color = 'white';
+para5.innerHTML = 'Yes I did it !! I manipulated the DOM and styled  my elements 🤩🤩🤩';
+
